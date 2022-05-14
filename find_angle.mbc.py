6e8869f4690984ebe5d8f2997a0ebc55889fb396
yakob <99880602+yakobsolo@users.noreply.github.com>
2022-05-14 12:25:06 +0300

    Create find_angle.mbc.py

---
+++ find_angle.mbc.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUa
+import math
+AB = int(input())
+BC = int(input())
+print(round(math.degrees(math.atan(AB / BC))), chr(176), sep = '')
