74bf2902762204348f01f5e9c14e840bb36100a5
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2022-08-25 13:35:50 +0300

    sort color using buble sort

--- week2/sort_colors.py
+++ week2/sort_colors.py
@@ -1,8 +1,14 @@
 class Solution:
+    """ 
+    Solution: has method to sort color
+    """
     def sortColors(self, nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
-        """
+        sorts the color using buble sort
+        attribute:
+            nums: list of colors
+            """
+   
         for i in range(0, len(nums) - 1):
             for j in range(0, len(nums) - 1):
                 if nums[j] > nums[j + 1]:
