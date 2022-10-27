d5b8809c76fbd1a0a732b8a6f1011149ba7a4003
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2022-10-27 13:25:29 +0300

    maximum point u can obtain from cards

---
+++ week-11/1423. Maximum Points You Can Obtain from Cards/Maximum_Points_You_Can_Obtain_from_Cards.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        size = len(cardPoints) - k
+        minSubArraySum = float('inf')
+        j = curr = 0
+        
+        for i, v in enumerate(cardPoints):
+            curr += v
+            if i - j + 1 > size:
+                curr -= cardPoints[j]
+                j += 1
+            if i - j + 1 == size:    
+                minSubArraySum = min(minSubArraySum, curr)
+				
+        return sum(cardPoints) - minSubArraySum
